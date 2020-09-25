7ce316f3e259efd541e397f5d7fdcc06cb6a3b8c
zirpav <zir-pav@mail.ru>
2020-09-25 09:33:30 +0300

    Два hello

---
+++ src/com/zir/jb2/HelloWorld2.java
@@ -0,0 +1,7 @@
+package com.zir.jb2;
+
+public class HelloWorld2 {
+    public static void main(String[] args) {
+        System.out.println("hello, world 2");
+    }
+}
---
+++ src/com/zir/jb2/hellowworld1.java
@@ -0,0 +1,7 @@
+package com.zir.jb2;
+
+public class hellowworld1 {
+    public static void main(String[] args) {
+        System.out.println("yoyoyo");
+    }
+}
